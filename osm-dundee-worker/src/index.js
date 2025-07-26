49ffe0f266b419cbeb519e9be761c0da38fce97c
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2025-07-26 13:43:22 +0000

    chore: CORS part 2

--- osm-dundee-worker/src/index.js
+++ osm-dundee-worker/src/index.js
@@ -1,13 +1,15 @@
 import yaml from 'yaml';
 
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type'
+};
+
 function handleOptions(request) {
 	return new Response(null, {
 		status: 204,
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
-			'Access-Control-Allow-Headers': 'Content-Type',
-		},
+		headers: corsHeaders
 	});
 }
 
@@ -17,7 +19,6 @@ export default {
 		if (request.method === 'OPTIONS') {
 			return handleOptions(request);
 		}
-
 
 		const yamlUrl = `https://raw.githubusercontent.com/OpenDataScotland/dundee_osm_health/refs/heads/main/site/_reports/cafes.yml?cachebust=${Date.now()}`;
 
@@ -44,7 +45,7 @@ export default {
 			const json = await overpassRes.json();
 
 			return new Response(JSON.stringify(json, null, 2), {
-				headers: { 'Content-Type': 'application/json', 'test': 'jcg' }
+				headers: { 'Content-Type': 'application/json', ...corsHeaders },
 			});
 		} catch (err) {
 			return new Response(`Error: ${err.message}`, { status: 500 });
